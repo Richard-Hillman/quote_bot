40dfe0d3e00480b71e12280a7fdd56ab6a5b5556
richardhillman <richard.cc.hillman@gmail.com>
2022-01-05 22:34:04 -0800

    added loading function to getquotes and newquote function

--- template_script.js
+++ template_script.js
@@ -3,11 +3,26 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote-button');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+// show loading 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// show completed hide loader
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 function newQuote() {
     // picking random number
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    loading();
     // check if author field is blank
     if (!quote.author) {
         quoteAuthor.textContent = 'Unknown';
@@ -22,17 +37,19 @@ function newQuote() {
     }
 
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get quotes from API 
 async function getQuotes() {
+    loading();
     const apiURL = 'https://type.fit/api/quotes'
     try{
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
-        console.error('no quotes available')
+        console.error('No quotes available')
     }
 }
 
@@ -48,3 +65,4 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // on load get quotes
 getQuotes();
+
