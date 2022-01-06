9a8c62cb2b8d877a9ba733ca1d3aa48c64b935f8
richardhillman <richard.cc.hillman@gmail.com>
2022-01-05 16:14:18 -0800

    added random quote functionality: dynamically reloads both quote text and author text upon reload of page

--- template_script.js
+++ template_script.js
@@ -1,8 +1,16 @@
-let apiQuotes = []
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter-button');
+const newQuoteBtn = document.getElementById('new-quote-button');
+
+let apiQuotes = [];
+
 function newQuote() {
     // picking random number
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
+    quoteAuthor.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 // Get quotes from API 
 async function getQuotes() {
@@ -12,6 +20,7 @@ async function getQuotes() {
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
+        console.error('no quotes available')
     }
 }
 
